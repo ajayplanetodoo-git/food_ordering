189ddbcf813f9223de6244e28aa47cc503420e7d
Ajay Prajapati <ajayplanetodoo@gmail.com>
2025-11-23 23:20:36 +0530

    admin control for customeuser admin

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username","first_name","email","role","is_active")
+    ordering = ("-date_joined",)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User,CustomUserAdmin)
