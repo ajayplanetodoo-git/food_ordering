9e88b327168cc8376df44850621cf29fdc7397e2
Ajay Prajapati <ajay@example.com>
2025-11-29 17:57:38 +0530

    vendor app and  model creation

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_accounts'
+    'user_accounts',
+    'vendor_app',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home , name="home"),
     path("accounts/", include("user_accounts.urls")),
+    path("vendor/",include("vendor_app.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
