8ec3c24a79b3a574c9e895b3be482c5d823e5495
Ajay Prajapati <ajay@example.com>
2026-07-08 17:38:11 +0530

    api function create for create user in serilizer

--- user_accounts/serializer.py
+++ user_accounts/serializer.py
@@ -6,8 +6,18 @@ from .models import User ,UserProfile
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name','last_name','username','email','phone_number','role']
+        fields = ['first_name','last_name','username','email','password','phone_number','role']
 
+    def create(self, validated_data):
+        user = User.objects.create_user(username=validated_data['username'],
+                                        first_name=validated_data['first_name'],
+                                        last_name=validated_data['last_name'],
+                                        phone_number = validated_data['phone_number'],
+                                        role=validated_data['role'],
+                                        email=validated_data['email'],
+                                        password=validated_data['password'])
+
+        return user
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
